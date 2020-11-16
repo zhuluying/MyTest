8a90dd4fd94756607de2e6b03eb051b750bc3226
zhuly <282647196@qq.com>
2020-11-16 17:39:34 +0800

    第一次提交

---
+++ MavenTest/src/main/java/One.java
@@ -0,0 +1,12 @@
+public class One {
+
+    public void DaYin(){
+        System.out.println("随便打印个什么");
+    }
+
+    public static void main(String[] args) {
+        One o = new One();
+        o.DaYin();
+
+    }
+}
